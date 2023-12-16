30b4c53f7122f64da37676a8e56c056017658a79
TreeBronch <74438588+TreeBronch@users.noreply.github.com>
2023-12-15 21:23:20 -0500

    Add project files.

---
+++ AnalysisService/Communication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnalysisService
+{
+    class Communication
+    {
+        public void Communication()
+        {
+
+        }
+    }
+}
